2baa44889f722b3a57045926c26bbc2352a7cd6d
jconnollyweb <johnc1983@hotmail.co.uk>
2024-03-22 10:56:39 +0000

    return cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express")
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000
 const { Sequelize, DataTypes } = require("sequelize");
 app.use(express.json())
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -126,7 +128,7 @@ sequelize
 // // const port = process.env.PORT || 3001
 
 // const app = express();
-// app.use(cors());
+app.use(cors());
 // app.use(bodyParser.json());
 
 // // Postgres client setup
