f282d5dee9eb62241d00b574b878d65b4a94137a
jconnollyweb <johnc1983@hotmail.co.uk>
2024-03-25 15:41:43 +0000

    will this work

--- client/src/pages/details/RoleDetails.js
+++ client/src/pages/details/RoleDetails.js
@@ -55,7 +55,9 @@ return (
         <div className="left"> 
           <p>Account: {values.account}</p>
           <p>Engagement: {values.engagement}</p>
+          {Array.isArray(values.role) && (
           <p>Role: {values.role.map((role, index) => <span key={index}>{role}<br /></span>)}</p>
+        )}
           <p>Start Date: {formatDate(values.startdate)} </p>
           <p>Owner: {values.owner} </p>
            <p>Revenue: £{values.revenue}</p>
@@ -66,7 +68,7 @@ return (
             <p>Sector: {values.sector}</p>
             <p>Location: {values.location}</p>            
             <p>Sales Channel: {values.channel} </p>
-            <p>Grades Wanted: {values.grade.map((grade, index) => <span key={index}>{grade}<br /></span>)}</p>
+           
             <p>End Date: {formatDate(values.enddate)} </p>
             <p>Originator: {values.originator} </p>
             <p>Sales Forecast: {values.forecast}</p>
