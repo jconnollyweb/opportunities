e9023c3c8c026474e64fb4ce38d2813548abbb07
jconnollyweb <johnc1983@hotmail.co.uk>
2024-03-26 15:26:42 +0000

    forecast

--- client/src/pages/details/RoleDetails.js
+++ client/src/pages/details/RoleDetails.js
@@ -73,8 +73,7 @@ return (
             <p>Grades Wanted: {JSON.parse(values.grade.replace(/"/g, '"').replace(/{/g, '[').replace(/}/g, ']')).map((grade, index) => <span key={index}>{grade}<br /></span>)}</p>)}
              <p>End Date: {formatDate(values.enddate)} </p>
             <p>Originator: {values.originator} </p>
-            {values.forecast && (
-           <p>Sales Forecast: {JSON.parse(values.forcast.replace(/"/g, '"').replace(/{/g, '[').replace(/}/g, ']')).map((forecast, index) => <span key={index}>{forecast}<br /></span>)}</p>)}
+            
 
          </div>
           {authenticated && (
