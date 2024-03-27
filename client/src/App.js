654ed93ee34f124180f831d2d33e333bb16ce480
jconnollyweb <johnc1983@hotmail.co.uk>
2024-03-27 15:33:31 +0000

    route edit role

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import AddOpportunities from "./pages/form/AddOpportunities";
 import RoleDetails from "./pages/details/RoleDetails";
 import Header from "./components/header/Header";
 import LoginPage from "./pages/login/LoginPage";
+import EditRole from "./components/edit/EditRole";
 // import GridPage from "./pages/grid/GridPage";
 
 
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/" element={ <OpenRoles />} />
           <Route path="/otherpage" element={< AddOpportunities />} />
           <Route path="/:id" element={ <RoleDetails /> } />
+          <Route path="/:id" element={ <EditRole /> } />
           {/* <Route path="/gridpage" element={<GridPage /> } /> */}
           <Route path="/login" element={<LoginPage />} />
           </Routes>
