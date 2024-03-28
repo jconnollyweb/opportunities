8a81d0622dd63ddd433aec7173b7d39fc0fc508a
jconnollyweb <johnc1983@hotmail.co.uk>
2024-03-28 14:45:32 +0000

    parse grades

--- client/src/components/filter/GradeFilter.js
+++ client/src/components/filter/GradeFilter.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './RoleFilter.css'
 
+
 function GradeFilter({ grades, onFilterChange }) {
   const [selectedGrades, setSelectedGrades] = useState([]);
 
@@ -12,98 +13,32 @@ function GradeFilter({ grades, onFilterChange }) {
       } else {
         return prevSelectedGrades.filter((grade) => grade !== gradeName)
       }
-    });
+    } )
+  
   };
 
+ 
   React.useEffect(() => {
     onFilterChange(selectedGrades);
   }, [selectedGrades, onFilterChange]);
 
-  // Function to parse JSON string grades to array
-  const parseGrades = (gradeString) => {
-    try {
-      return JSON.parse(gradeString.replace(/"/g, '"').replace(/{/g, '[').replace(/}/g, ']'));
-    } catch (error) {
-      console.error("Error parsing grades:", error);
-      return [];
-    }
-  };
-
   return (
     <div className="grade-filter-body">
-      <h2>Filter by Grade</h2>
-      {parseGrades(grades).map((grade) => ( // Use parseGrades to convert grades to array
-        <label className="filter-label" key={grade}>
-          <input
-            type="checkbox"
-            value={grade}
-            checked={selectedGrades.includes(grade)}
-            onChange={handleGradeChange}
-          />
-          {grade}
-          <br />
-        </label>
-      ))}
-    </div>
+    <h2>Filter by Grade</h2>
+    {JSON.parse(grades).map((grade, index) => (
+      <label className="filter-label" key={index}>
+        <input
+          type="checkbox"
+          value={grade}
+          checked={selectedGrades.includes(grade)}
+          onChange={handleGradeChange}
+        />
+        {grade}
+        <br />
+      </label>
+    ))}
+  </div>
   );
 }
 
 export default GradeFilter;
-
-
-
-// import React, { useState } from "react";
-// import './RoleFilter.css'
-
-
-// function GradeFilter({ grades, onFilterChange }) {
-//   const [selectedGrades, setSelectedGrades] = useState([]);
-
-//   const handleGradeChange = (event) => {
-//     const gradeName = event.target.value;
-//     setSelectedGrades((prevSelectedGrades) => {
-//       if (event.target.checked) {
-//         return [...prevSelectedGrades, gradeName]
-//       } else {
-//         return prevSelectedGrades.filter((grade) => grade !== gradeName)
-//       }
-//     } )
-  
-//   };
-
- 
-//   React.useEffect(() => {
-//     onFilterChange(selectedGrades);
-//   }, [selectedGrades, onFilterChange]);
-
-//   const parseGrades = (gradeString) => {
-//     try {
-//       return JSON.parse(gradeString.replace(/"/g, '"').replace(/{/g, '[').replace(/}/g, ']'));
-//     } catch (error) {
-//       console.error("Error parsing grades:", error);
-//       return [];
-//     }
-//   };
-
-
-//   return (
-//     <div className="grade-filter-body">
-//       <h2>Filter by Grade</h2>
-//       {parseGrades(grades).map((grade) => (
-//         <label className="filter-label" key={grade}>
-//           <input
-//             type="checkbox"
-//             value={grade}
-//             checked={selectedGrades.includes(grade)}
-//             onChange={handleGradeChange}
-//           />
-//           {grade}
-//           <br />
-//         </label>
-//       ))}
-    
-//     </div>
-//   );
-// }
-
-// export default GradeFilter;
